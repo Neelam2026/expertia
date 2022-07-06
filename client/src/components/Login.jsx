1c65921d3631a088409f457577a94b6025fbb012
Neelam <neelam0101singh@gmail.com>
2022-07-06 09:16:39 +0530

    added

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -84,7 +84,7 @@ export const Login=()=>{
                </Card.Body>
             </Card>
             <div className="w-100 text-center mt-2">
-               Need an account? <Link to="/signup">Sign Up</Link>
+               Need an account? <Link to="/register">Sign Up</Link>
             </div>
          </Container>
       </div>
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,7 +7,7 @@ import { AuthContextProvider } from "./Context/AuthContext";
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
   <BrowserRouter>
- <AuthContextProvider>
+  <AuthContextProvider>
   <App />
   </AuthContextProvider>
   </BrowserRouter>
